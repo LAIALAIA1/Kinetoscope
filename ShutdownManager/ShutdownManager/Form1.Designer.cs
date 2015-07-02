8c2c20cddeeb17a8981fcbe4ff0cd18f99af924d
unknown <kilian.brandtdi@SI-EIN-LAP-ISI7.intra.eiaj.ch>
2015-07-02 17:33:11 +0200

    shutdownmanager software

---
+++ ShutdownManager/ShutdownManager/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ShutdownManager
+{
+    partial class ShutdownManager
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBatteryState = new System.Windows.Forms.Label();
+            this.lblShutdownSeconds = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numTimeBeforeShutdown = new System.Windows.Forms.NumericUpDown();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeBeforeShutdown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Battery State";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shutdown in [s]:";
+            // 
+            // lblBatteryState
+            // 
+            this.lblBatteryState.AutoSize = true;
+            this.lblBatteryState.Location = new System.Drawing.Point(93, 28);
+            this.lblBatteryState.Name = "lblBatteryState";
+            this.lblBatteryState.Size = new System.Drawing.Size(62, 15);
+            this.lblBatteryState.TabIndex = 2;
+            this.lblBatteryState.Text = "undefined";
+            this.lblBatteryState.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblShutdownSeconds
+            // 
+            this.lblShutdownSeconds.AutoSize = true;
+            this.lblShutdownSeconds.Location = new System.Drawing.Point(111, 60);
+            this.lblShutdownSeconds.Name = "lblShutdownSeconds";
+            this.lblShutdownSeconds.Size = new System.Drawing.Size(62, 15);
+            this.lblShutdownSeconds.TabIndex = 3;
+            this.lblShutdownSeconds.Text = "undefined";
+            this.lblShutdownSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Time before shutdown [s]:";
+            // 
+            // numTimeBeforeShutdown
+            // 
+            this.numTimeBeforeShutdown.Location = new System.Drawing.Point(370, 26);
+            this.numTimeBeforeShutdown.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.numTimeBeforeShutdown.Minimum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numTimeBeforeShutdown.Name = "numTimeBeforeShutdown";
+            this.numTimeBeforeShutdown.Size = new System.Drawing.Size(120, 20);
+            this.numTimeBeforeShutdown.TabIndex = 5;
+            this.numTimeBeforeShutdown.Value = new decimal(new int[] {
+            1800,
+            0,
+            0,
+            0});
+            this.numTimeBeforeShutdown.ValueChanged += new System.EventHandler(this.numTimeBeforeShutdown_ValueChanged);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ShutdownManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 97);
+            this.Controls.Add(this.numTimeBeforeShutdown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblShutdownSeconds);
+            this.Controls.Add(this.lblBatteryState);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ShutdownManager";
+            this.Text = "Shutdown Manager 1.0";
+            this.Load += new System.EventHandler(this.ShutdownManager_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeBeforeShutdown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBatteryState;
+        private System.Windows.Forms.Label lblShutdownSeconds;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numTimeBeforeShutdown;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ ShutdownManager/ShutdownManager/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace ShutdownManager
+{
+    public partial class ShutdownManager : Form
+    {
+
+        public int elapsedTimeSinceNotCharging { get; set; }
+        public ShutdownManager()
+        {
+            InitializeComponent();
+        }
+
+        private void ShutdownManager_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+            handleNewBatteryState();
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            handleNewBatteryState();
+        }
+
+        private void handleNewBatteryState()
+        {
+            if ((SystemInformation.PowerStatus.BatteryChargeStatus & BatteryChargeStatus.Charging) == BatteryChargeStatus.Charging)
+            {
+                lblBatteryState.Text = "Charging, AC plugged";
+                resetShutdownTimer();
+            }
+            else
+            {
+                lblBatteryState.Text = "Discharging, AC unplugged";
+                launchShutdownTimer();
+            }
+        }
+
+        private void launchShutdownTimer()
+        {
+            timer.Start();
+        }
+
+        private void resetShutdownTimer()
+        {
+            lblShutdownSeconds.Text = "No shutdown planned";
+            timer.Stop();
+            elapsedTimeSinceNotCharging = 0;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTimeSinceNotCharging++;
+            lblShutdownSeconds.Text = (numTimeBeforeShutdown.Value - elapsedTimeSinceNotCharging) + "";
+            if(elapsedTimeSinceNotCharging > numTimeBeforeShutdown.Value)
+            {
+                shutdown();
+            }
+        }
+
+        private void shutdown()
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        private void numTimeBeforeShutdown_ValueChanged(object sender, EventArgs e)
+        {
+            elapsedTimeSinceNotCharging = 0;
+        }
+    }
+}
